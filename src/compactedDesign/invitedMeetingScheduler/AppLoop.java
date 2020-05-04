72dd533212a5af1e954363eb174117d247e870e2
Jeffrey Jiang <43833907+MechaDL7@users.noreply.github.com>
2020-05-04 16:12:24 -0400

    Update AppLoop.java

--- src/compactedDesign/invitedMeetingScheduler/AppLoop.java
+++ src/compactedDesign/invitedMeetingScheduler/AppLoop.java
@@ -1,5 +1,15 @@
 package compactedDesign.invitedMeetingScheduler;
 
-public class AppLoop {
+public class AppLoop implements Runnable{
+	
+	public AppLoop() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
