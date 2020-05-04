ea952dae71c1191e1ee7a6c948ec8bbe9d001e2e
Jeffrey Jiang <43833907+MechaDL7@users.noreply.github.com>
2020-05-04 11:58:09 -0400

    Added AppLoop and InputHandler
    Added to the project necessary components involved in making the program function.

--- src/compactedDesign/invitedMeetingScheduler/AppLauncher.java
+++ src/compactedDesign/invitedMeetingScheduler/AppLauncher.java
@@ -1,10 +1,26 @@
 package compactedDesign.invitedMeetingScheduler;
 
-public class AppLauncher {
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
-	public static void main(String[] args) {
+public class AppLauncher extends Application {
+
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub
-
+		
 	}
+	
+	/**
+	 * Starting method. 
+	 * @param args Used for launch method
+	 */
+	public static void main(String[] args) {launch(args);}
 
 }
---
+++ src/compactedDesign/invitedMeetingScheduler/AppLoop.java
@@ -0,0 +1,5 @@
+package compactedDesign.invitedMeetingScheduler;
+
+public class AppLoop {
+
+}
