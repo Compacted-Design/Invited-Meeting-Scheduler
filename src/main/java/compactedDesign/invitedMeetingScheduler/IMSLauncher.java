53542c4c6fad12047a2047f9d7ddcc8b66d5b8af
Jeffrey Jiang <43833907+MechaDL7@users.noreply.github.com>
2020-05-07 20:05:05 -0400

    Reorganized Project for Gradle
    Changed the project from a java project to a Gradle project, so it can use Google Sheets API

--- src/main/java/compactedDesign/invitedMeetingScheduler/IMSLauncher.java
+++ src/main/java/compactedDesign/invitedMeetingScheduler/IMSLauncher.java
@@ -1,23 +1,18 @@
 package compactedDesign.invitedMeetingScheduler;
 
 import javafx.application.Application;
-import javafx.scene.Group;
-import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 /**
- * <h1> Application Launcher </h1>
+ * <h1> Invited Meeting Scheduler Launcher </h1>
  * 
- * <p>The Application Launcher program initializes 
+ * <p>The Invited Meeting Scheduler Launcher program initializes 
  * the application.</p>
  * 
  * @author Jeffrey Jiang
  * @version 0.1
- * @since 2020-01-12
+ * @since 2020-05-07
  */
-public class AppLauncher extends Application {
+public class IMSLauncher extends Application {
 
 
 	@Override
@@ -25,7 +20,7 @@ public class AppLauncher extends Application {
 		//Setting up the window
 		primaryStage.setTitle("Invited Meeting Scheduler");//sets the title of the window
 		primaryStage.setOnCloseRequest(e -> System.exit(0)); // stops the program upon closing the window
-		
+		System.out.println(1);
 	}
 	
 	/**
