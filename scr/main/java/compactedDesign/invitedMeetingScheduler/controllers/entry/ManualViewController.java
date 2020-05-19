f5dab9dfceacd6538a871c38397f3d18baf7bf06
Jeffrey Jiang <43833907+MechaDL7@users.noreply.github.com>
2020-05-19 11:06:08 -0400

    Added data storage
    Created an excel sheet to store data

--- scr/main/java/compactedDesign/invitedMeetingScheduler/controllers/entry/ManualViewController.java
+++ scr/main/java/compactedDesign/invitedMeetingScheduler/controllers/entry/ManualViewController.java
@@ -1,5 +1,16 @@
 package compactedDesign.invitedMeetingScheduler.controllers.entry;
 
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
@@ -20,9 +31,42 @@ public class ManualViewController {
 	@FXML
 	private Button submitButton;
 	
+	@SuppressWarnings("resource")
 	@FXML
-	private void submitButtonClick() {
+	private void submitButtonClick() throws FileNotFoundException, IOException {
+		FileInputStream in = new FileInputStream(new File("scr/main/resources/data/Data.xlsx"));
+		Workbook wb = new XSSFWorkbook(in);
+		Sheet s = wb.getSheet("TestData");
+		int i = 1;
 		
+		for(; s.getRow(i) != null; i++) { //TODO: Change the cell values to equal what the cell titles are
+			if((""+((int)s.getRow(i).getCell(0).getNumericCellValue())).trim().equals(idEntry.getText().trim())) {
+				s.getRow(i).getCell(1).setCellValue(firstNameEntry.getText().trim());
+				s.getRow(i).getCell(2).setCellValue(lastNameEntry.getText().trim());
+				s.getRow(i).getCell(3).setCellValue(middleSchoolEntry.getText().trim());
+				if(smcsCheck.isSelected()) {
+					s.getRow(i).getCell(4).setCellValue("Y");
+				}else {
+					s.getRow(i).getCell(4).setCellValue("N");
+				}
+				if(globalCheck.isSelected()) {
+					s.getRow(i).getCell(5).setCellValue("Y");
+				}else {
+					s.getRow(i).getCell(5).setCellValue("N");
+				}
+				if(humanitiesCheck.isSelected()) {
+					s.getRow(i).getCell(6).setCellValue("Y");
+				}else {
+					s.getRow(i).getCell(6).setCellValue("N");
+				}
+				break;
+			}
+		}
+		in.close();
+		FileOutputStream out = new FileOutputStream(new File("scr/main/resources/data/RawData.xlsx"));
+		wb.write(out);
+		out.close();
+		wb.close();
 	}
 
 }
